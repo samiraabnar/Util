1c471a33fe2339883398e376239bf97cf077d0e1
Samira Abnar <samiraabnar@gmail.com>
2016-03-01 15:00:56 +0100

    Adding the sigmoid function to MathUtil

--- util/math/MathUtil.py
+++ util/math/MathUtil.py
@@ -1,6 +1,19 @@
 import numpy as np
+
+
 class MathUtil(object):
 
     def softmax(x):
-        xt = np.exp(x - np.max(x))
-        return xt / np.sum(xt)
+        e_x = np.exp(x - np.max(x))
+        return e_x / np.sum(e_x)
+
+    def sigmoid(x):
+        return 1.0 / (1 + np.exp(-x))
+
+
+if __name__ == '__main__':
+    a = np.array([1,2,3])
+    b = MathUtil.sigmoid(a)
+    print(b)
+
+
